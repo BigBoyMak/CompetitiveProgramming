213559ebf188563c5def14bc077213bda20ae0cf
BigBoyMak <106482710+BigBoyMak@users.noreply.github.com>
2023-12-16 14:55:41 +0530

    Update Codechef.java
    queue system

--- Codechef.java
+++ Codechef.java
@@ -2,25 +2,59 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-public class Codechef
+class Codechef
 {
+    
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		  BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		
 		// StringTokenizer st = new StringTokenizer(br.readLine()); 
 		// int n = Integer.parseInt(st.nextToken()); 
 		// int k = Integer.parseInt(st.nextToken()); 
 
 		//input logic
+		Queue<Processor> queue = new LinkedList<>();
 		
-		int n = Integer.parseInt(br.readLine()); 
-		char[] arr = br.readLine().toCharArray();
-		System.out.println(process(arr, n));
+		int t = Integer.parseInt(br.readLine());
+		while(t-- > 0){
+		    //read each test case input
+		    //process input and add to processor class
+		    queue.add(new Processor());
+		}
+		
+		while(!queue.isEmpty()){
+		    Processor curr = queue.poll();
+		    System.out.println(curr.getResult());
+		}
 	}
 	
-	public static int process(char[] arr, int n){
-	    //problem logic
-	}
+
+}
+
+class Processor{
+    //sample input 
+    int[] map;
+    int[] colours;
+    
+    Processor(int[] map, int[] colours){
+        this.map = map;
+        this.colours = colours;
+    }
+    
+    public int getResult(){
+        //core logic
+        int ans = 0;
+	    int prev = -1;
+	    for(int i=0; i<colours.length; i++){
+	        int currColor = colours[i];
+	        int curr = map[currColor];
+	        if(prev != curr){
+	            ans++;
+	        }
+	        prev = curr;
+	    }
+	    return ans;
+    }
 }
