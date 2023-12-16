2610c2e59088e4605cbb8abc3656e23c92ad7ef6
BigBoyMak <106482710+BigBoyMak@users.noreply.github.com>
2023-12-16 14:52:33 +0530

    Create CodeForces.java

---
+++ CodeForces.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class CodeForces
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		// StringTokenizer st = new StringTokenizer(br.readLine()); 
+		// int n = Integer.parseInt(st.nextToken()); 
+		// int k = Integer.parseInt(st.nextToken()); 
+
+		//input logic
+		
+		int n = Integer.parseInt(br.readLine()); 
+		char[] arr = br.readLine().toCharArray();
+		System.out.println(process(arr, n));
+	}
+	
+	public static int process(char[] arr, int n){
+	    //problem logic
+	}
+}
