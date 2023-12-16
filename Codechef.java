c011e2a1a6c2c40f32898e81d105144fa79330f7
BigBoyMak <106482710+BigBoyMak@users.noreply.github.com>
2023-12-16 13:55:17 +0530

    Update Codechef.java

--- Codechef.java
+++ Codechef.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Codechef
+public class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
@@ -12,17 +12,15 @@ class Codechef
 		// StringTokenizer st = new StringTokenizer(br.readLine()); 
 		// int n = Integer.parseInt(st.nextToken()); 
 		// int k = Integer.parseInt(st.nextToken()); 
+
+		//input logic
 		
-		int t = Integer.parseInt(br.readLine()); 
-		long[] arr = new long[t];
-		for(int i=0; i<t; i++){
-		    long n = Long.parseLong(br.readLine());
-		    arr[i] = n;
-		}
-		
-		for(long n: arr){
-		    long out = 1L + ((n-2) * (n-1));
-		    System.out.println(out);
-		}
+		int n = Integer.parseInt(br.readLine()); 
+		char[] arr = br.readLine().toCharArray();
+		System.out.println(process(arr, n));
+	}
+	
+	public static int process(char[] arr, int n){
+	    //problem logic
 	}
 }
